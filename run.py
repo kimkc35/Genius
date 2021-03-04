a1f933a1e7f6c2af29cf0ad638884f24d11e27e3
kimkc35 <42907356+kimkc35@users.noreply.github.com>
2021-03-04 11:07:36 +0900

    Update run.py

--- run.py
+++ run.py
@@ -14,5 +14,5 @@ async def on_message(message):
     if message.content.startswith("%짱재"): 
         await message.channel.send("멍청이") 
 
-
-client.run("ODE2ODMyODMwNjc5NDE2ODgw.YEAs2g.6lKRtOqsk6UMMWEfsTjZIwlNCc0")
+access_token = os.environ["token"]
+client.run(access_token)
