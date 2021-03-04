c039ceffaf2b7605823f6c81da3a73dc7e8b10f7
kimkc35 <kimkc35@naver.com>
2021-03-04 10:43:43 +0900

    .

--- run.py
+++ run.py
@@ -1,7 +1,12 @@
 import discord
 import asyncio
+import os
 
 client = discord.Client()
+
+@client.event
+async def on_ready():
+    await client.change_presence(status=discord.Status.online)
 
 @client.event
 async def on_message(message):
